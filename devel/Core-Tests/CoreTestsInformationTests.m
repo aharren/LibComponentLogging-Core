8155f972c1343c4cb4ad33723b5e3e74720ee5b5
Arne Harren <ah@0xc0.de>
2011-10-16 17:30:27 +0200

    version 1.1.5

--- devel/Core-Tests/CoreTestsInformationTests.m
+++ devel/Core-Tests/CoreTestsInformationTests.m
@@ -39,7 +39,7 @@
 #define _(cstring) ((cstring == NULL) ? @"(null)" : [NSString stringWithUTF8String:cstring])
 
 - (void)testInformationVersion {
-    STAssertEqualObjects(_(_lcl_version), @"1.1.4", nil);
+    STAssertEqualObjects(_(_lcl_version), @"1.1.5", nil);
 }
 
 @end
