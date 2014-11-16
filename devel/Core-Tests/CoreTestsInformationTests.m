42c6bdea6c58ad8a5d2a49e598163fba5862b5f0
Arne Harren <ah@0xc0.de>
2014-11-16 10:42:29 +0100

    1.4.0-dev

--- devel/Core-Tests/CoreTestsInformationTests.m
+++ devel/Core-Tests/CoreTestsInformationTests.m
@@ -39,7 +39,7 @@
 #define _(cstring) ((cstring == NULL) ? @"(null)" : [NSString stringWithUTF8String:cstring])
 
 - (void)testInformationVersion {
-    AssertEqualObjects(_(_lcl_version), @"1.3.3");
+    AssertEqualObjects(_(_lcl_version), @"1.4.0-dev");
 }
 
 @end
